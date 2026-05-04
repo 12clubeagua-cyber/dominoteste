fcd60a814d7ae6709b21769656010bb62a2056b0
12clubeagua-cyber <12clubeagua@gmail.com>
2026-05-04 01:23:56 -0300

    Update config.js

--- config.js
+++ config.js
@@ -10,11 +10,11 @@ const CONFIG = Object.freeze({
   // Bloco de configurações do comportamento dos Bots (IA)
   BOT: {
     // Tempo mínimo de espera (em milissegundos) antes do bot jogar.
-    // Isso simula o tempo de reação de um humano (0.9 segundos).
-    MIN_DELAY: 900,
+    // Isso simula o tempo de reação de um humano
+    MIN_DELAY: 1,
     
-    // Tempo máximo de espera (1.3 segundos) para adicionar aleatoriedade na "demora" do bot.
-    MAX_DELAY: 1300,
+    // Tempo máximo de espera para adicionar aleatoriedade na "demora" do bot.
+    MAX_DELAY: 2,
     
     // O texto que vai aparecer no painel de status enquanto ocorre esse atraso simulado.
     THINKING_MSG: " PENSANDO..."
@@ -22,26 +22,26 @@ const CONFIG = Object.freeze({
   
   // Bloco de configurações gerais de proporção, visual e tempos do jogo
   GAME: {
-    // Largura padrão de uma peça na matemática de renderização (18 unidades/pixels base).
-    TILE_W: 18,           // Largura da peca (lado curto)
+    // Largura padrão de uma peça na matemática de renderização
+    TILE_W: 36,           // Largura da peca (lado curto)
     
-    // Comprimento padrão da peça. Observe que é exatamente o dobro da largura (18x2 = 36).
-    TILE_L: 36,           // Comprimento da peca (lado longo)
+    // Comprimento padrão da peça.
+    TILE_L: 72,           // Comprimento da peca (lado longo)
     
-    // Tempo que a tela de resultado/fim de rodada fica exposta para os jogadores lerem (provavelmente em segundos).
-    RESULT_DISPLAY_TIME: 7,
+    // Tempo que a tela de resultado/fim de rodada fica exposta para os jogadores lerem em segundos
+    RESULT_DISPLAY_TIME: 5,
     
-    // Tempo em milissegundos (2.5s) que a mensagem ou efeito de "Passou a vez" fica visível na tela.
+    // Tempo em milissegundos que a mensagem ou efeito de "Passou a vez" fica visível na tela.
     PASS_DISPLAY_TIME: 2500,
     
-    // Um breve atraso de 1200ms (1.2s) antes de liberar a primeira jogada, para os jogadores se situarem após o embaralhamento.
-    START_DELAY: 1200,
+    // Um breve atraso antes de liberar a primeira jogada, para os jogadores se situarem após o embaralhamento.
+    START_DELAY: 1,
     
     // Limite de zoom máximo que a câmera do tabuleiro ("cobrinha") pode dar. 1.2 significa 120% do tamanho normal.
     SNAKE_MAX_SCALE: 1.2,
     
     // Limite máximo de peças empilhadas na vertical indo em linha reta antes de forçar a "cobrinha" a dobrar de direção (pra não sair da mesa).
-    MAX_VERT: 6,
+    MAX_VERT: 5,
     
     // Limite máximo de peças na horizontal em linha reta antes de forçar a curva.
     MAX_HORIZ: 2
